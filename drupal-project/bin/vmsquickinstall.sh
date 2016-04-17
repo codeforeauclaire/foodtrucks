9bd06666851710a9aaa77ea03b5e93ca26f4900a
Anthony <Anthony123272@gmail.com>
2016-04-17 08:27:07 -0500

    Setup swap

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -6,6 +6,18 @@
 
 { # this ensures the entire script is downloaded and run #
 
+# Setup swap (which will enable on reboot)
+# * We need more than 512MB ram for Meteor to work. Warning: meteor may be slow, especially first run, on small instances
+# * https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+echo "/swapfile   none    swap    sw    0   0" >> "/etc/fstab"
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+# * Enable swap file now before reboot
+sudo swapon /swapfile
+
 # Update all software & install new
 sudo apt-get update && sudo apt-get upgrade -y
 
