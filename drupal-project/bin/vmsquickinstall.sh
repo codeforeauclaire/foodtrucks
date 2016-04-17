f401e490c960507fade673cf92becbfc9649570e
Anthony <Anthony123272@gmail.com>
2016-04-17 08:00:28 -0500

    Install composer, I think this works (tested manually)

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -37,6 +37,12 @@ sudo apt-get install -y php5
 # Install PHPMySQL support
 sudo apt-get install -y php5-mysql
 
+# Install composer
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
 # Clone repository
 git clone https://github.com/codeforeauclaire/foodtrucks.git /root/foodtrucks
 
