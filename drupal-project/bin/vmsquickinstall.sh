87c67a2b800561a8ccaa6b5c5ebeca8342ca67de
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-05-03 14:21:26 -0500

    Update vmsquickinstall.sh
    Composer installation instructions update

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -49,10 +49,10 @@ sudo apt-get install -y php5
 # Install PHP extensions
 sudo apt-get install -y php5-mysql php5-curl php5-gd php5-fpm
 
-# Install composer
+# Install composer - https://getcomposer.org/download/
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
-php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a52be7b8724e47499b039d53415953cc3d5b459b9d9c0308301f867921c19efc623b81dfef8fc2be194a5cf56945d223') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 # Install drush (Instructiosn from http://docs.drush.org/en/master/install/)
