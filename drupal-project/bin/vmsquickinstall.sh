0ef5a464969782f7a126d7f7046da5d257407c45
Anthony <Anthony123272@gmail.com>
2016-05-09 21:10:50 -0500

    Closes issue #14

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -51,8 +51,9 @@ sudo apt-get install -y php5-mysql php5-curl php5-gd php5-fpm
 
 # Install composer - https://getcomposer.org/download/
 # * TODO: Figure out a real way to do this as page says not to distribute the installation code, this is a hack
+# ** Commented out SHA check as a hack so this stops breaking so much
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+# php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
