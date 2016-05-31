0722af325af70e704429ebb967c4c00d6c3ca82a
Lowell <lowell@digitalfarrier.com>
2016-05-31 12:51:15 -0500

    moving custom modules and theme into main repository

---
+++ drupal-project/web/themes/custom/foodtruckstheme/config.rb
@@ -0,0 +1,34 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+css_dir = "css" # Compiled CSS goes here.
+sass_dir = "scss"
+images_dir = "images-source"
+generated_images_dir = "images" # Generated images (e.g. sprites) go here.
+javascripts_dir = "js"
+output_style = :expanded # Can also be :nested or :compact or :compressed
+relative_assets = true # True to generate relative paths from CSS to assets.
+line_comments = true # Controls debugging comments that display the original location of your selectors.
+
+sourcemap = false
+
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+cache_path = "/tmp/.sass-cache"
