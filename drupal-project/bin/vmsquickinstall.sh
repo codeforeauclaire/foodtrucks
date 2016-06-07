a2c450693b2decea43b7b983d19fec63a011435f
Anthony <Anthony123272@gmail.com>
2016-06-07 11:46:04 -0500

    [Drupal] VMS: Streamlining installation
    A number of `apt-get install` and `apt-get update` commands combined.
    
    Hopefully I didn't mess anything up; this needs testing

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: Optomize this script?
-# TODO: * There's a lot of `sudo apt-get update` which may be combinable
-# TODO: ** May make less readable
-
 { # this ensures the entire script is downloaded and run #
 
 # Setup swap (which will enable on reboot)
@@ -18,40 +14,35 @@ echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 # * Enable swap file now before reboot
 sudo swapon /swapfile
 
-# Update all software & install new
+# Update all base software
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Install the simple stuff
+# Add custom repositories
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository 'deb [arch=amd64,i386] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main' -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+
+# Install packaged stuff
 # * Some versions from DO article @ https://goo.gl/jCiEhS
 # * Compass
 # ** We could compile Ruby - Digitial Ocean's instructions - https://goo.gl/TpZ2wL
 # ** We could install Ruby - https://www.brightbox.com/docs/ruby/ubuntu/
 # ** However we can just do `ruby-compass` much quicker, simpler, and more reliably with a single command (not doing ruby dev, we don't need fluff)
-sudo apt-get install -y git nginx ruby-compass
+# * PHP 5.6
+# ** https://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/
+# ** PHP extensions
+sudo apt-get install -y git nginx ruby-compass mariadb-server php5.6 php5-mysql php5-curl php5-gd php5-fpm php5.6-xml
 
-# Install Maria DB
+# Setup Maria DB
 # * Instructions from https://goo.gl/d1vOx6 (Ubuntu >> 14.04 >> 10.1 >> DigitalOcean - New York)
 # * No password for maria db install from http://dba.stackexchange.com/a/60192
-sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-sudo add-apt-repository 'deb [arch=amd64,i386] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main' -y
 sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
-sudo apt-get install -y mariadb-server
 mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('');"
-
-# Install PHP 5.6
-# * https://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-sudo apt-get install -y python-software-properties
-sudo apt-get update
-sudo apt-get install -y php5.6
-
-# Install PHP extensions
-sudo apt-get install -y php5-mysql php5-curl php5-gd php5-fpm php5.6-xml
 
 # Install composer - https://getcomposer.org/download/
 # * TODO: Figure out a real way to do this as page says not to distribute the installation code, this is a hack
