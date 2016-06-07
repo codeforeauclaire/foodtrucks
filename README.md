a9f9c5ba05f5911dff5fecc4d1a0c34df0be4546
Anthony <Anthony123272@gmail.com>
2016-06-07 12:53:32 -0500

    deploy-to-gh-pages run per #76

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
+
 # Food Trucks :: Consumer UI
 
 Displays where food trucks so consumers can get some food.
@@ -8,22 +10,23 @@ Use these instructions to setup a temporary* development environment of this pro
 
 1. Fork this repository
 1. Create a new [Virtual Machine](http://vms.codeforeauclaire.org/) >> SSH in >> Run vms quick install:
- 1. `export GHUSER='AnthonyAstige'` (Replace AnthonyAstige with your username)
- 1. `curl -L -o- https://rawgit.com/$GHUSER/foodtrucks/master/consumer-ui/bin/vmsquickinstall.sh | bash`
+    1. `export GHUSER='AnthonyAstige'` (Replace AnthonyAstige with your username)
+    1. `curl -L -o- https://rawgit.com/$GHUSER/foodtrucks/master/consumer-ui/bin/vmsquickinstall.sh | bash`
 1. Run app
- 1. `(cd ~/foodtrucks/consumer-ui && ./bin/serve)`
- 1. Load http://{vms-ip}:3000 in your browser
- 1. Edit a file >> restart ./bin/serve >> see changes in your browser
+    1. `(cd ~/foodtrucks/consumer-ui && ./bin/serve)`
+    1. Load http://{vms-ip}:3000 in your browser
+    1. Edit a file >> restart ./bin/serve >> see changes in your browser
 
 *For a permanent development environment we recommend you read the referenced script above to install locally.
 
 ## Development Notes
 
+### Misc
+
+* Don't work directly on the gh-pages branch, the script /consumer-ui/bin/deploy-to-gh-pages sends code there
 * Mapping via Leaflet http://leafletjs.com/
- * http://leafletjs.com/examples/mobile.html
- * Data: Static json data that will run
+    * http://leafletjs.com/examples/mobile.html
+    * Data: Static json data that will run
 * UI Components via JQuery Mobile
-* Hosting - Just client side. Use apache, use python, use whatever. Here are some quick methods
- * https://askubuntu.com/questions/377389/how-to-easily-start-a-webserver-in-any-folder
 * [Original draw.io mockup from mar 28th 2016 C4EC hack night](https://drive.google.com/file/d/0B1hUzWEXfF7oWHVPRGZhLVE3UDA/view)
 * [General Specs](../SPECS.md)
--- js/index.js
+++ js/index.js
@@ -1,67 +1,150 @@
-var API = "http://foodtrucks1.version-three.com/default-endpoint";
-//API = "js/test-data.json"; //uncomment to use local test data
+/* globals L, moment */
+var API_EVENTS = 'http://vendors.foodtrucks.codeforeauclaire.org/api/events'
+// API_EVENTS = "js/test/event.json"; // uncomment to use local test data
+var API_VENDORS = 'http://vendors.foodtrucks.codeforeauclaire.org/api/vendors'
+// API_VENDORS = "js/test/vendor.json"; // uncomment to use local test data
 
-$(document).on('pageshow', function() {
+$(document).on('pageshow', function () {
   var map = new L.Map('map', {
     center: new L.LatLng(44.799, -91.464),
     zoom: 12,
-    layers: [new L.StamenTileLayer("toner-lite")]
-  });
-
-  var markerCluster = L.markerClusterGroup();
-  var markers = new Array();
-
- $.getJSON({
-    dataType: "json",
-    url: API
+    layers: [new L.StamenTileLayer('terrain')]
   })
-  .done(function(data){
-    $.each(data, function(key, value) {
+
+  var markerCluster = L.markerClusterGroup()
+  map.addLayer(markerCluster)
+  var markers = []
+
+  var timeFormat = 'h:mma'
+  var dateFormat = 'M/D/YY'
+
+  var pastEventIcon = new L.Icon({
+    iconUrl: 'deps/leaflet/images/marker-icon-gray-2x.png',
+    shadowUrl: 'deps/leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
+  // current events use default marker
+
+  // get vendor list
+  $.getJSON({
+    dataType: 'json',
+    url: API_VENDORS
+  })
+  .done(function (data) {
+    $.each(data, function (key, value) {
+      $('#truck-list').append("<div><p><b><a href='" + (value.website_url || '#') + "' target='_blank'>" +
+        value.title + '</a></b> ' + (value.description || '') + '</p></div>')
+    })
+  })
+  .fail(function (err) {
+    console.log(err)
+  })
+
+  // get event list
+  $.getJSON({
+    dataType: 'json',
+    url: API_EVENTS
+  })
+  .done(function (data) {
+    $.each(data, function (key, value) {
       if (value.lat) {
+        // Adjust API which look like UTC but are actually CDT
+        var expired = isExpired(value.end_time)
         var marker = new L.Marker(new L.LatLng(value.lat, value.lng))
-          .bindPopup('<a href="' + value.links + '">' + value.name + '</a>'+
-              '<p>Hours: ' + value.start_time + ' - ' + value.end_time + '</p>');
-        markerCluster.addLayer(marker);
+          .bindPopup(getPopupHtml(value, dateFormat, timeFormat, expired))
+
+        if (expired)marker.setIcon(pastEventIcon)
         marker.start_time = value.start_time
         marker.end_time = value.end_time
         markers.push(marker)
       }
-    });
+    })
+    if (window.location.hash.toLowerCase() === '#all') {
+      $('#dateFilter').val('All')
+      markerCluster.addLayers(markers)
+      if (markerCluster._topClusterLevel.getChildCount() === 0) {
+        $('#message').popup('open')
+      } else {
+        map.fitBounds(markerCluster.getBounds())
+      }
+    } else {
+      $('#dateFilter').trigger('change')
+    }
   })
-  .fail(function(err) {
-    console.log( err );
+  .fail(function (err) {
+    console.log(err)
   })
-
-  map.addLayer(markerCluster);
 
   $('#datepicker a').click(function (e) {
-    e.preventDefault();
-    var dateField = $(this).siblings('div').children('input');
-    var date = new Date(dateField.datepicker('getDate'));
+    e.preventDefault()
+    var dateField = $(this).siblings('div').children('input')
+    var date = new Date(dateField.datepicker('getDate'))
     if ($(this).children('.left-arrow').size() > 0) {
-      dateField.datepicker('setDate', new Date(date.setDate(date.getDate() - 1)));
+      dateField.datepicker('setDate', new Date(date.setDate(date.getDate() - 1)))
     } else {
-      dateField.datepicker('setDate', new Date(date.setDate(date.getDate() + 1)));
+      dateField.datepicker('setDate', new Date(date.setDate(date.getDate() + 1)))
     }
-    $( "#dateFilter" ).trigger( "change" );
-  });
+    $('#dateFilter').trigger('change')
+  })
 
+  // temporary client side date filter. To be replaced with ajax call to server action.
   $('#dateFilter').change(function (e) {
-    var filterByDate = $(this).datepicker('getDate');
-    var filterByStart = new Date(filterByDate)
-    var filterByEnd = new Date(filterByDate)
-    filterByEnd.setDate (filterByEnd.getDate()+1);
+    var filterDate = $(this).datepicker('getDate')
+    // convert to UTC to match api dates
+    filterDate = new Date(filterDate.getUTCFullYear(), filterDate.getUTCMonth(), filterDate.getUTCDate(),
+      filterDate.getUTCHours(), filterDate.getUTCMinutes(), filterDate.getUTCSeconds())
 
-    markerCluster.clearLayers();
-    for (var i=0;i<markers.length;i++){
-      //console.log(filterByStart, new Date(markers[i].start_time), new Date(markers[i].end_time), filterByEnd)
-      // bug in date format, timezone, or logic
-      if(new Date(markers[i].start_time ) >= filterByStart
-        && new Date (markers[i].end_time) < filterByEnd  ){
-          markerCluster.addLayer(markers[i]);
+    markerCluster.clearLayers()
+    for (var i = 0; i < markers.length; i++) {
+      var open = new Date(markers[i].start_time)
+      var close = new Date(markers[i].end_time)
+      if (isBefore(open, filterDate) && isAfter(close, filterDate)) {
+        markerCluster.addLayer(markers[i])
       }
     }
-  });
 
-});
+    if (markerCluster._topClusterLevel.getChildCount() === 0) {
+      $('#message').popup('open')
+    } else {
+      map.fitBounds(markerCluster.getBounds())
+    }
+  })
+})
 
+// compare dates without time, return true if date1 is on or before date2
+function isBefore (date1, date2) {
+  if (date1.getFullYear() < date2.getFullYear()) return true
+  else if (date1.getFullYear() <= date2.getFullYear() && date1.getMonth() < date2.getMonth()) return true
+  else if (date1.getFullYear() <= date2.getFullYear() && date1.getMonth() <= date2.getMonth() && date1.getDate() <= date2.getDate()) return true
+  else return false
+}
+// compare dates without time, return true if date1 is on or after date2
+function isAfter (date1, date2) {
+  if (date1.getFullYear() > date2.getFullYear()) return true
+  else if (date1.getFullYear() >= date2.getFullYear() && date1.getMonth() > date2.getMonth()) return true
+  else if (date1.getFullYear() >= date2.getFullYear() && date1.getMonth() >= date2.getMonth() && date1.getDate() >= date2.getDate()) return true
+  else return false
+}
+
+function getPopupHtml (value, dateFormat, timeFormat, expired) {
+  var content = '<a href="' + (value[0].website_url ? value[0].website_url : '#') + '">' + value[0].title + '</a><br/>'
+  if (value[0].logo) content += '<img src="' + value[0].logo + '" alt="logo"/>'
+  content += '<p>' + value[0].description + '</p>'
+
+  content += '<p class="details">Here on ' +
+    moment(value.start_time).format(dateFormat) + ' <br><strong>' +
+    moment(value.start_time).format(timeFormat) + ' - ' +
+    moment(value.end_time).format(timeFormat) + ' ' +
+        (moment().isDST() ? '' : '') +
+        '</strong></p>'
+  if (expired) content += '<strong class="ended">(Ended)</strong>'
+  return content
+}
+
+// Takes end_time in format given by api
+function isExpired (endtime) {
+  return new Date() > new Date(endtime + (moment().isDST() ? '-05:00' : '-06:00'))
+}
