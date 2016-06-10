282c9e8595eb70f56c2a2baf43db0bacea33b5d1
lowell <lowell@digitalfarrier.com>
2016-06-09 22:45:23 -0500

    php-mbstring

--- drupal-project/bin/vmsquickinstall.sh
+++ drupal-project/bin/vmsquickinstall.sh
@@ -40,7 +40,7 @@ sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_
 # * PHP 5.6
 # ** https://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/
 # ** PHP extensions
-sudo apt-get install -y git nginx ruby-compass mariadb-server php php-mysql php-curl php-gd php-fpm php-xml php-curl php-xdebug drush
+sudo apt-get install -y git nginx ruby-compass mariadb-server php php-mysql php-curl php-gd php-fpm php-xml php-curl php-xdebug drush php-mbstring
 sed -i 's/zend_extension=xdebug.so/#zend_extension=xdebug.so/' /etc/php/7.0/mods-available/xdebug.ini
 
 composer global require "hirak/prestissimo:^0.3"
