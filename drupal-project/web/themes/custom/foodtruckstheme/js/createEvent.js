a7d425ced2a50f0df2b876d4ba95c149a4040985
Anthony <Anthony123272@gmail.com>
2016-06-11 18:10:11 -0400

    Select input on focus for #64
    Waiting for map to exist was hacked in, there's possibly a better 'Druplier' way, or deeper plugin integration.

--- drupal-project/web/themes/custom/foodtruckstheme/js/createEvent.js
+++ drupal-project/web/themes/custom/foodtruckstheme/js/createEvent.js
@@ -1,3 +1,27 @@
 /**
  * Created by lowell on 6/11/16.
+ *
+ * Initialized by Anthony on 6/11/16
  */
+;(function($) {
+	$(document).ready(function() {
+		// Wait until element exists
+		// * http://stackoverflow.com/a/13709125/4747661
+		// ** https://gist.github.com/PizzaBrandon/5709010
+		// TODO: Hook into map creation correctly and remove this hack?
+		var geoInputSel = '.geocode-controlls-wrapper > input';
+		$(geoInputSel).waitUntilExists(function() {
+			// Setup
+			var geoInputEl = jQuery(geoInputSel)
+
+			// Select all when focused (to easily type more)
+			geoInputEl.focus(function() { $(this).select() })
+			
+			// Prevent mouse up unfocusing
+			// http://stackoverflow.com/a/1269767/4747661
+			geoInputEl.mouseup(function(e){
+				e.preventDefault()
+			})
+		})
+	});
+}(jQuery))
